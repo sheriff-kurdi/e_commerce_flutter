3c1c1d4539926e0d1f65bfbd251f3194e01c2f76
Kurdi <sheriff.kurdi@gmail.com>
2022-04-05 08:47:35 +0200

    design

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -18,40 +18,49 @@ class _LoginState extends State<Login> {
     AuthService authService = AuthService();
 
 
-    return  Center(
-      child: Column(
-
-        mainAxisAlignment: MainAxisAlignment.center,
-        children:  [
-          Visibility(
-            child: const Text("Failed Login"),
-            visible: !isSuccessLogin,
-
-          ),
-          TextField(
-            controller: userNameController,
-            decoration: const InputDecoration(
-              hintText: 'User Name',
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(38.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:  [
+            Visibility(
+              child: const Text("Failed Login"),
+              visible: !isSuccessLogin,
             ),
-          ),
-          TextField(
-            controller: passwordController,
-            decoration: const InputDecoration(
-              hintText: 'Password',
+            Visibility(
+              child: const Text("Login Succeed"),
+              visible: isSuccessLogin,
             ),
-          ),
-          ElevatedButton(
-            child: const Text("Login"),
-            onPressed:() async {
-              isSuccessLogin = await authService.login(userNameController.text, passwordController.text);
-
-              setState(()  {
-                isSuccessLogin = isSuccessLogin;
-                print("login ${isSuccessLogin}" );
-              });
-            },
-          )
-        ],
+            TextField(
+              controller: userNameController,
+              decoration: const InputDecoration(
+                hintText: 'User Name',
+              ),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(
+                hintText: 'Password',
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+              child: const Text("Login"),
+              onPressed:() async {
+                isSuccessLogin = await authService.login(userNameController.text, passwordController.text);
+                setState(()  {
+                  isSuccessLogin = isSuccessLogin;
+                });
+              },
+            )
+          ],
+        ),
       ),
     );
   }
