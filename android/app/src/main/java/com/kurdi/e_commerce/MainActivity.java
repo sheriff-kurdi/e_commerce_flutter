fc456e4f8a54d836ac8f21e463414c460f70a163
Kurdi <sheriff.kurdi@gmail.com>
2022-04-05 08:38:06 +0200

    Initial commit

---
+++ android/app/src/main/java/com/kurdi/e_commerce/MainActivity.java
@@ -0,0 +1,6 @@
+package com.kurdi.e_commerce;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:e_commerce/screens/home.dart';
+import 'package:e_commerce/screens/login.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body:  Login(),
+      ),
+    );
+  }
+}
